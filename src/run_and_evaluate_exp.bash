acde427b5206c8812a430cbfc5e7eebece51ab67
Geraint Palmer <palmer.geraint@googlemail.com>
2026-03-27 15:43:46 +0000

    ready for experiments

---
+++ src/run_and_evaluate_exp.bash
@@ -0,0 +1,3 @@
+python3 run_qlearning.py experiments/$1
+python3 evaluate_qlearning.py experiments/$1
+python3 analyse_evaluation.py experiments/$1
--- src/run_and_evaluate_exp1.bash
+++
@@ -1,2 +0,0 @@
-python3 run_qlearning.py experiments/exp2
-python3 evaluate_qlearning.py experiments/exp2
