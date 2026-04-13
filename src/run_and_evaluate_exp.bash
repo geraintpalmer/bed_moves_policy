2b8eb59d323bea74fd242feff09a61d8516b23cb
Geraint Palmer <palmer.geraint@googlemail.com>
2026-04-13 16:54:24 +0100

    add nthreds to run_and_evaluate script

--- src/run_and_evaluate_exp.bash
+++ src/run_and_evaluate_exp.bash
@@ -1,6 +1,6 @@
 SECONDS=0
-python3 training.py experiments/$1
-python3 evaluation.py experiments/$1
+python3 training.py experiments/$1 $2
+python3 evaluation.py experiments/$1 $2
 python3 analysis.py experiments/$1
 duration=$SECONDS
 echo "Time duration: $duration seconds"
