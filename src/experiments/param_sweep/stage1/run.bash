b79fc33ff02f6922773eb2abe1f81ce03c7d28c5
Geraint Palmer <palmer.geraint@googlemail.com>
2026-08-12 14:13:19 +0100

    set up stage 1 parameter sweep

---
+++ src/experiments/param_sweep/stage1/run.bash
@@ -0,0 +1,79 @@
+echo "Alpha: 0.2; Gamma: 0.6"
+python training.py experiments/param_sweep/stage1/alpha02_gamma06/ 5
+python evaluation.py experiments/param_sweep/stage1/alpha02_gamma06/ 5
+python analysis.py experiments/param_sweep/stage1/alpha02_gamma06/
+
+echo "Alpha: 0.2; Gamma: 0.7"
+python training.py experiments/param_sweep/stage1/alpha02_gamma07/ 5
+python evaluation.py experiments/param_sweep/stage1/alpha02_gamma07/ 5
+python analysis.py experiments/param_sweep/stage1/alpha02_gamma07/
+
+echo "Alpha: 0.2; Gamma: 0.8"
+python training.py experiments/param_sweep/stage1/alpha02_gamma08/ 5
+python evaluation.py experiments/param_sweep/stage1/alpha02_gamma08/ 5
+python analysis.py experiments/param_sweep/stage1/alpha02_gamma08/
+
+echo "Alpha: 0.2; Gamma: 0.9"
+python training.py experiments/param_sweep/stage1/alpha02_gamma09/ 5
+python evaluation.py experiments/param_sweep/stage1/alpha02_gamma09/ 5
+python analysis.py experiments/param_sweep/stage1/alpha02_gamma09/
+
+echo "Alpha: 0.4; Gamma: 0.6"
+python training.py experiments/param_sweep/stage1/alpha04_gamma06/ 5
+python evaluation.py experiments/param_sweep/stage1/alpha04_gamma06/ 5
+python analysis.py experiments/param_sweep/stage1/alpha04_gamma06/
+
+echo "Alpha: 0.4; Gamma: 0.7"
+python training.py experiments/param_sweep/stage1/alpha04_gamma07/ 5
+python evaluation.py experiments/param_sweep/stage1/alpha04_gamma07/ 5
+python analysis.py experiments/param_sweep/stage1/alpha04_gamma07/
+
+echo "Alpha: 0.4; Gamma: 0.8"
+python training.py experiments/param_sweep/stage1/alpha04_gamma08/ 5
+python evaluation.py experiments/param_sweep/stage1/alpha04_gamma08/ 5
+python analysis.py experiments/param_sweep/stage1/alpha04_gamma08/
+
+echo "Alpha: 0.4; Gamma: 0.9"
+python training.py experiments/param_sweep/stage1/alpha04_gamma09/ 5
+python evaluation.py experiments/param_sweep/stage1/alpha04_gamma09/ 5
+python analysis.py experiments/param_sweep/stage1/alpha04_gamma09/
+
+echo "Alpha: 0.6; Gamma: 0.6"
+python training.py experiments/param_sweep/stage1/alpha06_gamma06/ 5
+python evaluation.py experiments/param_sweep/stage1/alpha06_gamma06/ 5
+python analysis.py experiments/param_sweep/stage1/alpha06_gamma06/
+
+echo "Alpha: 0.6; Gamma: 0.7"
+python training.py experiments/param_sweep/stage1/alpha06_gamma07/ 5
+python evaluation.py experiments/param_sweep/stage1/alpha06_gamma07/ 5
+python analysis.py experiments/param_sweep/stage1/alpha06_gamma07/
+
+echo "Alpha: 0.6; Gamma: 0.8"
+python training.py experiments/param_sweep/stage1/alpha06_gamma08/ 5
+python evaluation.py experiments/param_sweep/stage1/alpha06_gamma08/ 5
+python analysis.py experiments/param_sweep/stage1/alpha06_gamma08/
+
+echo "Alpha: 0.6; Gamma: 0.9"
+python training.py experiments/param_sweep/stage1/alpha06_gamma09/ 5
+python evaluation.py experiments/param_sweep/stage1/alpha06_gamma09/ 5
+python analysis.py experiments/param_sweep/stage1/alpha06_gamma09/
+
+echo "Alpha: 0.8; Gamma: 0.6"
+python training.py experiments/param_sweep/stage1/alpha08_gamma06/ 5
+python evaluation.py experiments/param_sweep/stage1/alpha08_gamma06/ 5
+python analysis.py experiments/param_sweep/stage1/alpha08_gamma06/
+
+echo "Alpha: 0.8; Gamma: 0.7"
+python training.py experiments/param_sweep/stage1/alpha08_gamma07/ 5
+python evaluation.py experiments/param_sweep/stage1/alpha08_gamma07/ 5
+python analysis.py experiments/param_sweep/stage1/alpha08_gamma07/
+
+echo "Alpha: 0.8; Gamma: 0.8"
+python training.py experiments/param_sweep/stage1/alpha08_gamma08/ 5
+python evaluation.py experiments/param_sweep/stage1/alpha08_gamma08/ 5
+python analysis.py experiments/param_sweep/stage1/alpha08_gamma08/
+
+echo "Alpha: 0.8; Gamma: 0.9"
+python training.py experiments/param_sweep/stage1/alpha08_gamma09/ 5
+python evaluation.py experiments/param_sweep/stage1/alpha08_gamma09/ 5
+python analysis.py experiments/param_sweep/stage1/alpha08_gamma09/
