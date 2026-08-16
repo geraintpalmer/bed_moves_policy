4c8c5e3a124c73708ddca4220e6cd0208acd15c1
Geraint Palmer <palmer.geraint@googlemail.com>
2026-08-16 11:46:40 +0100

    write up preliminary results more to add

---
+++ src/experiments/param_sweep/round1/run.bash
@@ -0,0 +1,16 @@
+alphas=("0.2" "0.4" "0.6" "0.8")
+gammas=("0.6" "0.7" "0.8" "0.9")
+
+n_cores="$1"
+
+for alpha in "${alphas[@]}"; do
+    for gamma in "${gammas[@]}"; do
+        alpha_str=$(echo "$alpha" | tr -d '.')
+        gamma_str=$(echo "$gamma" | tr -d '.')
+        dir="experiments/param_sweep/round1/alpha${alpha_str}_gamma${gamma_str}/"
+        echo "Alpha: ${alpha}; Gamma: ${gamma}"
+        python training.py "$dir" "$n_cores"
+        python evaluation.py "$dir" "$n_cores"
+        python analysis.py "$dir"
+    done
+done
