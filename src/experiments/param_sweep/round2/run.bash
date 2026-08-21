3057bfe041bb37c2874ba8268bb7d9addc6c328a
Geraint Palmer <palmer.geraint@googlemail.com>
2026-08-21 19:43:35 +0100

    complete round 1, prep round 2

---
+++ src/experiments/param_sweep/round2/run.bash
@@ -0,0 +1,75 @@
+echo "Alpha=0.4, Gamma=0.81"
+python training.py experiments/param_sweep/round2/alpha040_gamma81/ $1
+python evaluation.py experiments/param_sweep/round2/alpha040_gamma81/ $1
+echo "Alpha=0.4, Gamma=0.83"
+python training.py experiments/param_sweep/round2/alpha040_gamma83/ $1
+python evaluation.py experiments/param_sweep/round2/alpha040_gamma83/ $1
+echo "Alpha=0.4, Gamma=0.85"
+python training.py experiments/param_sweep/round2/alpha040_gamma85/ $1
+python evaluation.py experiments/param_sweep/round2/alpha040_gamma85/ $1
+echo "Alpha=0.4, Gamma=0.87"
+python training.py experiments/param_sweep/round2/alpha040_gamma87/ $1
+python evaluation.py experiments/param_sweep/round2/alpha040_gamma87/ $1
+echo "Alpha=0.4, Gamma=0.89"
+python training.py experiments/param_sweep/round2/alpha040_gamma89/ $1
+python evaluation.py experiments/param_sweep/round2/alpha040_gamma89/ $1
+echo "Alpha=0.45, Gamma=0.81"
+python training.py experiments/param_sweep/round2/alpha045_gamma81/ $1
+python evaluation.py experiments/param_sweep/round2/alpha045_gamma81/ $1
+echo "Alpha=0.45, Gamma=0.83"
+python training.py experiments/param_sweep/round2/alpha045_gamma83/ $1
+python evaluation.py experiments/param_sweep/round2/alpha045_gamma83/ $1
+echo "Alpha=0.45, Gamma=0.85"
+python training.py experiments/param_sweep/round2/alpha045_gamma85/ $1
+python evaluation.py experiments/param_sweep/round2/alpha045_gamma85/ $1
+echo "Alpha=0.45, Gamma=0.87"
+python training.py experiments/param_sweep/round2/alpha045_gamma87/ $1
+python evaluation.py experiments/param_sweep/round2/alpha045_gamma87/ $1
+echo "Alpha=0.45, Gamma=0.89"
+python training.py experiments/param_sweep/round2/alpha045_gamma89/ $1
+python evaluation.py experiments/param_sweep/round2/alpha045_gamma89/ $1
+echo "Alpha=0.5, Gamma=0.81"
+python training.py experiments/param_sweep/round2/alpha050_gamma81/ $1
+python evaluation.py experiments/param_sweep/round2/alpha050_gamma81/ $1
+echo "Alpha=0.5, Gamma=0.83"
+python training.py experiments/param_sweep/round2/alpha050_gamma83/ $1
+python evaluation.py experiments/param_sweep/round2/alpha050_gamma83/ $1
+echo "Alpha=0.5, Gamma=0.85"
+python training.py experiments/param_sweep/round2/alpha050_gamma85/ $1
+python evaluation.py experiments/param_sweep/round2/alpha050_gamma85/ $1
+echo "Alpha=0.5, Gamma=0.87"
+python training.py experiments/param_sweep/round2/alpha050_gamma87/ $1
+python evaluation.py experiments/param_sweep/round2/alpha050_gamma87/ $1
+echo "Alpha=0.5, Gamma=0.89"
+python training.py experiments/param_sweep/round2/alpha050_gamma89/ $1
+python evaluation.py experiments/param_sweep/round2/alpha050_gamma89/ $1
+echo "Alpha=0.55, Gamma=0.81"
+python training.py experiments/param_sweep/round2/alpha055_gamma81/ $1
+python evaluation.py experiments/param_sweep/round2/alpha055_gamma81/ $1
+echo "Alpha=0.55, Gamma=0.83"
+python training.py experiments/param_sweep/round2/alpha055_gamma83/ $1
+python evaluation.py experiments/param_sweep/round2/alpha055_gamma83/ $1
+echo "Alpha=0.55, Gamma=0.85"
+python training.py experiments/param_sweep/round2/alpha055_gamma85/ $1
+python evaluation.py experiments/param_sweep/round2/alpha055_gamma85/ $1
+echo "Alpha=0.55, Gamma=0.87"
+python training.py experiments/param_sweep/round2/alpha055_gamma87/ $1
+python evaluation.py experiments/param_sweep/round2/alpha055_gamma87/ $1
+echo "Alpha=0.55, Gamma=0.89"
+python training.py experiments/param_sweep/round2/alpha055_gamma89/ $1
+python evaluation.py experiments/param_sweep/round2/alpha055_gamma89/ $1
+echo "Alpha=0.6, Gamma=0.81"
+python training.py experiments/param_sweep/round2/alpha060_gamma81/ $1
+python evaluation.py experiments/param_sweep/round2/alpha060_gamma81/ $1
+echo "Alpha=0.6, Gamma=0.83"
+python training.py experiments/param_sweep/round2/alpha060_gamma83/ $1
+python evaluation.py experiments/param_sweep/round2/alpha060_gamma83/ $1
+echo "Alpha=0.6, Gamma=0.85"
+python training.py experiments/param_sweep/round2/alpha060_gamma85/ $1
+python evaluation.py experiments/param_sweep/round2/alpha060_gamma85/ $1
+echo "Alpha=0.6, Gamma=0.87"
+python training.py experiments/param_sweep/round2/alpha060_gamma87/ $1
+python evaluation.py experiments/param_sweep/round2/alpha060_gamma87/ $1
+echo "Alpha=0.6, Gamma=0.89"
+python training.py experiments/param_sweep/round2/alpha060_gamma89/ $1
+python evaluation.py experiments/param_sweep/round2/alpha060_gamma89/ $1
