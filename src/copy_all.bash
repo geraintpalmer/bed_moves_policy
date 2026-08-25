da6bd87fdfe8d3e9d68a844d0f0fade77b35806c
Geraint Palmer <palmer.geraint@googlemail.com>
2026-08-25 21:18:37 +0100

    write up zeta exp; setup robust experiments

--- src/copy_all.bash
+++ src/copy_all.bash
@@ -1,4 +1,2 @@
-scp geraint@harpy.maths.cf.ac.uk:bed_moves_policy/src/experiments/main_run/results/robust_evaluation.csv experiments/main_run/results/
-scp geraint@harpy.maths.cf.ac.uk:bed_moves_policy/src/experiments/zeta_experiment/zeta15/results/evaluation.csv experiments/zeta_experiment/zeta15/results/
-scp geraint@harpy.maths.cf.ac.uk:bed_moves_policy/src/experiments/zeta_experiment/zeta85/results/evaluation.csv experiments/zeta_experiment/zeta85/results/
-scp geraint@harpy.maths.cf.ac.uk:bed_moves_policy/src/experiments/scheme_experiment/epsilon_greedy/results/evaluation.csv experiments/scheme_experiment/epsilon_greedy/results/
+scp geraint@harpy.maths.cf.ac.uk:bed_moves_policy/src/experiments/robustness_experiments/0_to_02/results/evaluation.csv experiments/robustness_experiments/0_to_02/results/
+scp geraint@harpy.maths.cf.ac.uk:bed_moves_policy/src/experiments/robustness_experiments/0_to_05/results/evaluation.csv experiments/robustness_experiments/0_to_05/results/
--- src/experiments/robustness_experiments/run.bash
+++ src/experiments/robustness_experiments/run.bash
@@ -1,11 +1,13 @@
 echo "From 0.0 to 0.5"
-mkdir experiments/robustness_experiments/0_to_05/results
-mkdir experiments/robustness_experiments/0_to_05/results/tmp
-python training.py experiments/robustness_experiments/0_to_05/ $1
-python evaluation.py experiments/robustness_experiments/0_to_05/ $1
+# mkdir experiments/robustness_experiments/0_to_05/results
+# mkdir experiments/robustness_experiments/0_to_05/results/tmp
+# python training.py experiments/robustness_experiments/0_to_05/ $1
+# python evaluation.py experiments/robustness_experiments/0_to_05/ $1
+python robust_evaluation.py experiments/robustness_experiments/0_to_05/ $1 15
 
 echo "From 0.0 to 0.2"
-mkdir experiments/robustness_experiments/0_to_02/results
-mkdir experiments/robustness_experiments/0_to_02/results/tmp
-python training.py experiments/robustness_experiments/0_to_02/ $1
-python evaluation.py experiments/robustness_experiments/0_to_02/ $1
+# mkdir experiments/robustness_experiments/0_to_02/results
+# mkdir experiments/robustness_experiments/0_to_02/results/tmp
+# python training.py experiments/robustness_experiments/0_to_02/ $1
+# python evaluation.py experiments/robustness_experiments/0_to_02/ $1
+python robust_evaluation.py experiments/robustness_experiments/0_to_02/ $1 15
