03b4a4a9c818de6877abe0ba4f011023c2ccf8a1
Geraint Palmer <palmer.geraint@googlemail.com>
2026-08-25 14:18:18 +0100

    fix typo

--- src/experiments/robustness_experiments/run.bash
+++ src/experiments/robustness_experiments/run.bash
@@ -1,11 +1,11 @@
 echo "From 0.0 to 0.5"
-mkdir experiments/robustnedd_experiment/0_to_05/results
-mkdir experiments/robustnedd_experiment/0_to_05/results/tmp
-python training.py experiments/robustnedd_experiment/0_to_05/ $1
-python evaluation.py experiments/robustnedd_experiment/0_to_05/ $1
+mkdir experiments/robustness_experiment/0_to_05/results
+mkdir experiments/robustness_experiment/0_to_05/results/tmp
+python training.py experiments/robustness_experiment/0_to_05/ $1
+python evaluation.py experiments/robustness_experiment/0_to_05/ $1
 
 echo "From 0.0 to 0.2"
-mkdir experiments/robustnedd_experiment/0_to_02/results
-mkdir experiments/robustnedd_experiment/0_to_02/results/tmp
-python training.py experiments/robustnedd_experiment/0_to_02/ $1
-python evaluation.py experiments/robustnedd_experiment/0_to_02/ $1
+mkdir experiments/robustness_experiment/0_to_02/results
+mkdir experiments/robustness_experiment/0_to_02/results/tmp
+python training.py experiments/robustness_experiment/0_to_02/ $1
+python evaluation.py experiments/robustness_experiment/0_to_02/ $1
--- src/experiments/run.bash
+++
@@ -1,3 +0,0 @@
-python robust_evaluation.py experiments/main_run/ $1 12
-bash experiments/zeta_experiment/run.bash $1
-bash experiments/scheme_experiment/run.bash $1
