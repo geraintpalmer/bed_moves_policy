902b35227f07bccc1abed1386588bab7fe3f7182
Geraint Palmer <palmer.geraint@googlemail.com>
2026-08-25 14:08:30 +0100

    ran and analysed zeta experiments

--- src/experiments/scheme_experiment/run.bash
+++
@@ -1,5 +0,0 @@
-echo "Scheme = Epsilon-Greedy"
-mkdir experiments/scheme_experiment/epsilon_greedy/results
-mkdir experiments/scheme_experiment/epsilon_greedy/results/tmp
-python training.py experiments/scheme_experiment/epsilon_greedy/ $1
-python evaluation.py experiments/scheme_experiment/epsilon_greedy/ $1
