677c7149e83f47b12c24d79468fdb9111127ee5e
Geraint Palmer <palmer.geraint@googlemail.com>
2026-08-26 23:17:58 +0100

    write up everything

--- src/copy_all.bash
+++ src/copy_all.bash
@@ -1,1 +1,2 @@
-scp geraint@harpy.maths.cf.ac.uk:bed_moves_policy/src/experiments/final_run/results/evaluation.csv experiments/final_run/results/
+scp geraint@harpy.maths.cf.ac.uk:bed_moves_policy/src/experiments/final_run/results/robust_evaluation.csv experiments/final_run/results/
+scp geraint@harpy.maths.cf.ac.uk:bed_moves_policy/src/experiments/final_run41/results/evaluation.csv experiments/final_run41/results/
