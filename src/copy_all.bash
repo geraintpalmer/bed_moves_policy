9faa7d18070a58c451b2319da6362c04d1ecb829
Geraint Palmer <palmer.geraint@googlemail.com>
2026-08-26 13:15:58 +0100

    run more pruning experiments

--- src/copy_all.bash
+++ src/copy_all.bash
@@ -1,7 +1,7 @@
-scp geraint@harpy.maths.cf.ac.uk:bed_moves_policy/src/experiments/pruning_experiments/h001/results/evaluation.csv experiments/pruning_experiments/h001/results/
-scp geraint@harpy.maths.cf.ac.uk:bed_moves_policy/src/experiments/pruning_experiments/h002/results/evaluation.csv experiments/pruning_experiments/h002/results/
-scp geraint@harpy.maths.cf.ac.uk:bed_moves_policy/src/experiments/pruning_experiments/h005/results/evaluation.csv experiments/pruning_experiments/h005/results/
-scp geraint@harpy.maths.cf.ac.uk:bed_moves_policy/src/experiments/pruning_experiments/h010/results/evaluation.csv experiments/pruning_experiments/h010/results/
-scp geraint@harpy.maths.cf.ac.uk:bed_moves_policy/src/experiments/pruning_experiments/h025/results/evaluation.csv experiments/pruning_experiments/h025/results/
-scp geraint@harpy.maths.cf.ac.uk:bed_moves_policy/src/experiments/pruning_experiments/h050/results/evaluation.csv experiments/pruning_experiments/h050/results/
-scp geraint@harpy.maths.cf.ac.uk:bed_moves_policy/src/experiments/pruning_experiments/h100/results/evaluation.csv experiments/pruning_experiments/h100/results/
+scp geraint@harpy.maths.cf.ac.uk:bed_moves_policy/src/experiments/pruning_experiments/h001/results/robust_evaluation.csv experiments/pruning_experiments/h001/results/
+scp geraint@harpy.maths.cf.ac.uk:bed_moves_policy/src/experiments/pruning_experiments/h002/results/robust_evaluation.csv experiments/pruning_experiments/h002/results/
+scp geraint@harpy.maths.cf.ac.uk:bed_moves_policy/src/experiments/pruning_experiments/h005/results/robust_evaluation.csv experiments/pruning_experiments/h005/results/
+scp geraint@harpy.maths.cf.ac.uk:bed_moves_policy/src/experiments/pruning_experiments/h010/results/robust_evaluation.csv experiments/pruning_experiments/h010/results/
+scp geraint@harpy.maths.cf.ac.uk:bed_moves_policy/src/experiments/pruning_experiments/h025/results/robust_evaluation.csv experiments/pruning_experiments/h025/results/
+scp geraint@harpy.maths.cf.ac.uk:bed_moves_policy/src/experiments/pruning_experiments/h050/results/robust_evaluation.csv experiments/pruning_experiments/h050/results/
+scp geraint@harpy.maths.cf.ac.uk:bed_moves_policy/src/experiments/pruning_experiments/h100/results/robust_evaluation.csv experiments/pruning_experiments/h100/results/
--- src/experiments/pruning_experiments/run.bash
+++ src/experiments/pruning_experiments/run.bash
@@ -1,55 +1,90 @@
-echo "h = 0"
+# echo "h = 0"
 # mkdir experiments/pruning_experiments/h000/results
 # mkdir experiments/pruning_experiments/h000/results/tmp
 # python training.py experiments/pruning_experiments/h000/ $1
 # python evaluation.py experiments/pruning_experiments/h000/ $1
-python robust_evaluation.py experiments/pruning_experiments/h001/ $1 12
+# python robust_evaluation.py experiments/pruning_experiments/h001/ $1 12
 
-echo "h = 1"
+# echo "h = 1"
 # mkdir experiments/pruning_experiments/h001/results
 # mkdir experiments/pruning_experiments/h001/results/tmp
 # python training.py experiments/pruning_experiments/h001/ $1
 # python evaluation.py experiments/pruning_experiments/h001/ $1
-python robust_evaluation.py experiments/pruning_experiments/h001/ $1 15
+# python robust_evaluation.py experiments/pruning_experiments/h001/ $1 15
 
-echo "h = 5"
+# echo "h = 5"
 # mkdir experiments/pruning_experiments/h005/results
 # mkdir experiments/pruning_experiments/h005/results/tmp
 # python training.py experiments/pruning_experiments/h005/ $1
 # python evaluation.py experiments/pruning_experiments/h005/ $1
-python robust_evaluation.py experiments/pruning_experiments/h005/ $1 21
+# python robust_evaluation.py experiments/pruning_experiments/h005/ $1 21
 
-echo "h = 25"
+# echo "h = 25"
 # mkdir experiments/pruning_experiments/h025/results
 # mkdir experiments/pruning_experiments/h025/results/tmp
 # python training.py experiments/pruning_experiments/h025/ $1
 # python evaluation.py experiments/pruning_experiments/h025/ $1
-python robust_evaluation.py experiments/pruning_experiments/h025/ $1 21
+# python robust_evaluation.py experiments/pruning_experiments/h025/ $1 21
 
-echo "h = 100"
+# echo "h = 100"
 # mkdir experiments/pruning_experiments/h100/results
 # mkdir experiments/pruning_experiments/h100/results/tmp
 # python training.py experiments/pruning_experiments/h100/ $1
 # python evaluation.py experiments/pruning_experiments/h100/ $1
-python robust_evaluation.py experiments/pruning_experiments/h100/ $1 21
+# python robust_evaluation.py experiments/pruning_experiments/h100/ $1 21
 
-echo "h = 50"
+# echo "h = 50"
 # mkdir experiments/pruning_experiments/h050/results
 # mkdir experiments/pruning_experiments/h050/results/tmp
 # python training.py experiments/pruning_experiments/h050/ $1
 # python evaluation.py experiments/pruning_experiments/h050/ $1
-python robust_evaluation.py experiments/pruning_experiments/h050/ $1 21
+# python robust_evaluation.py experiments/pruning_experiments/h050/ $1 21
 
-echo "h = 2"
+# echo "h = 2"
 # mkdir experiments/pruning_experiments/h002/results
 # mkdir experiments/pruning_experiments/h002/results/tmp
 # python training.py experiments/pruning_experiments/h002/ $1
 # python evaluation.py experiments/pruning_experiments/h002/ $1
-python robust_evaluation.py experiments/pruning_experiments/h002/ $1 20
+# python robust_evaluation.py experiments/pruning_experiments/h002/ $1 20
 
-echo "h = 10"
+# echo "h = 10"
 # mkdir experiments/pruning_experiments/h010/results
 # mkdir experiments/pruning_experiments/h010/results/tmp
 # python training.py experiments/pruning_experiments/h010/ $1
 # python evaluation.py experiments/pruning_experiments/h010/ $1
-python robust_evaluation.py experiments/pruning_experiments/h010/ $1 18
+# python robust_evaluation.py experiments/pruning_experiments/h010/ $1 18
+
+echo "h = 3"
+mkdir experiments/pruning_experiments/h003/results
+mkdir experiments/pruning_experiments/h003/results/tmp
+python training.py experiments/pruning_experiments/h003/ $1
+python evaluation.py experiments/pruning_experiments/h003/ $1
+# python robust_evaluation.py experiments/pruning_experiments/h003/ $1 15
+
+echo "h = 15"
+mkdir experiments/pruning_experiments/h015/results
+mkdir experiments/pruning_experiments/h015/results/tmp
+python training.py experiments/pruning_experiments/h015/ $1
+python evaluation.py experiments/pruning_experiments/h015/ $1
+# python robust_evaluation.py experiments/pruning_experiments/h015/ $1 15
+
+echo "h = 35"
+mkdir experiments/pruning_experiments/h015/results
+mkdir experiments/pruning_experiments/h015/results/tmp
+python training.py experiments/pruning_experiments/h015/ $1
+python evaluation.py experiments/pruning_experiments/h015/ $1
+# python robust_evaluation.py experiments/pruning_experiments/h015/ $1 15
+
+echo "h = 75"
+mkdir experiments/pruning_experiments/h075/results
+mkdir experiments/pruning_experiments/h075/results/tmp
+python training.py experiments/pruning_experiments/h075/ $1
+python evaluation.py experiments/pruning_experiments/h075/ $1
+# python robust_evaluation.py experiments/pruning_experiments/h075/ $1 15
+
+echo "h = 150"
+mkdir experiments/pruning_experiments/h150/results
+mkdir experiments/pruning_experiments/h150/results/tmp
+python training.py experiments/pruning_experiments/h150/ $1
+python evaluation.py experiments/pruning_experiments/h150/ $1
+# python robust_evaluation.py experiments/pruning_experiments/h150/ $1 15
