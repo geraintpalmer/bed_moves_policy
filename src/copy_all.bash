28a22ac7a9adb334e5c7923ebb237733415d39e1
Geraint Palmer <palmer.geraint@googlemail.com>
2026-08-26 19:37:09 +0100

    finish hyper-parameter experiments and write up

--- src/copy_all.bash
+++ src/copy_all.bash
@@ -1,7 +1,5 @@
-scp geraint@harpy.maths.cf.ac.uk:bed_moves_policy/src/experiments/pruning_experiments/h001/results/robust_evaluation.csv experiments/pruning_experiments/h001/results/
-scp geraint@harpy.maths.cf.ac.uk:bed_moves_policy/src/experiments/pruning_experiments/h002/results/robust_evaluation.csv experiments/pruning_experiments/h002/results/
-scp geraint@harpy.maths.cf.ac.uk:bed_moves_policy/src/experiments/pruning_experiments/h005/results/robust_evaluation.csv experiments/pruning_experiments/h005/results/
-scp geraint@harpy.maths.cf.ac.uk:bed_moves_policy/src/experiments/pruning_experiments/h010/results/robust_evaluation.csv experiments/pruning_experiments/h010/results/
-scp geraint@harpy.maths.cf.ac.uk:bed_moves_policy/src/experiments/pruning_experiments/h025/results/robust_evaluation.csv experiments/pruning_experiments/h025/results/
-scp geraint@harpy.maths.cf.ac.uk:bed_moves_policy/src/experiments/pruning_experiments/h050/results/robust_evaluation.csv experiments/pruning_experiments/h050/results/
-scp geraint@harpy.maths.cf.ac.uk:bed_moves_policy/src/experiments/pruning_experiments/h100/results/robust_evaluation.csv experiments/pruning_experiments/h100/results/
+scp geraint@harpy.maths.cf.ac.uk:bed_moves_policy/src/experiments/pruning_experiments/h003/results/robust_evaluation.csv experiments/pruning_experiments/h003/results/
+scp geraint@harpy.maths.cf.ac.uk:bed_moves_policy/src/experiments/pruning_experiments/h025/results/robust_evaluation.csv experiments/pruning_experiments/h015/results/
+scp geraint@harpy.maths.cf.ac.uk:bed_moves_policy/src/experiments/pruning_experiments/h035/results/robust_evaluation.csv experiments/pruning_experiments/h035/results/
+scp geraint@harpy.maths.cf.ac.uk:bed_moves_policy/src/experiments/pruning_experiments/h075/results/robust_evaluation.csv experiments/pruning_experiments/h075/results/
+scp geraint@harpy.maths.cf.ac.uk:bed_moves_policy/src/experiments/pruning_experiments/h150/results/robust_evaluation.csv experiments/pruning_experiments/h150/results/
