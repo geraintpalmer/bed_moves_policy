c0c3e89d2d2bb94f16bee38b4a7e85ee874100ed
Geraint Palmer <palmer.geraint@googlemail.com>
2026-08-26 21:02:01 +0100

    ready for robustness experiment

--- src/copy_all.bash
+++ src/copy_all.bash
@@ -1,5 +1,1 @@
-scp geraint@harpy.maths.cf.ac.uk:bed_moves_policy/src/experiments/pruning_experiments/h003/results/robust_evaluation.csv experiments/pruning_experiments/h003/results/
-scp geraint@harpy.maths.cf.ac.uk:bed_moves_policy/src/experiments/pruning_experiments/h025/results/robust_evaluation.csv experiments/pruning_experiments/h015/results/
-scp geraint@harpy.maths.cf.ac.uk:bed_moves_policy/src/experiments/pruning_experiments/h035/results/robust_evaluation.csv experiments/pruning_experiments/h035/results/
-scp geraint@harpy.maths.cf.ac.uk:bed_moves_policy/src/experiments/pruning_experiments/h075/results/robust_evaluation.csv experiments/pruning_experiments/h075/results/
-scp geraint@harpy.maths.cf.ac.uk:bed_moves_policy/src/experiments/pruning_experiments/h150/results/robust_evaluation.csv experiments/pruning_experiments/h150/results/
+scp geraint@harpy.maths.cf.ac.uk:bed_moves_policy/src/experiments/final_run/results/evaluation.csv experiments/final_run/results/
--- src/experiments/final_run/run.bash
+++ src/experiments/final_run/run.bash
@@ -1,4 +1,10 @@
-mkdir experiments/final_run/results
-mkdir experiments/final_run/results/tmp
-python training.py experiments/final_run/ $1
-python evaluation.py experiments/final_run/ $1
+# mkdir experiments/final_run/results
+# mkdir experiments/final_run/results/tmp
+# python training.py experiments/final_run/ $1
+# python evaluation.py experiments/final_run/ $1
+python robust_evaluation.py experiments/final_run/ $1 31
+
+mkdir experiments/final_run41/results
+mkdir experiments/final_run41/results/tmp
+python training.py experiments/final_run41/ $1
+python evaluation.py experiments/final_run41/ $1
